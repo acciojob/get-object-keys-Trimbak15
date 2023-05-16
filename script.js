2ea948ed6e8ffa7860cc2777910437ad4d21b4f8
Trimbak Pakhale <118504736+Trimbak15@users.noreply.github.com>
2023-05-16 18:33:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+// Creating the student object
+const student = {
+  name: "John Doe"
+};
+
+// Adding getKeys() to Object prototype
+Object.prototype.getKeys = function() {
+  return Object.keys(this);
+};
+
+// Using the getKeys() method
+const keys = student.getKeys();
+console.log(keys); // Output: ["name"]
